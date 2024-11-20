29543454270fc0f04d9eb439909bbc20a458be92
JonasNguyen <assasinhacker1@gmail.com>
2024-11-20 19:59:46 +0700

    Update file .gitlab-ci.yml

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -4,16 +4,18 @@ set -e # Dừng script nếu có lỗi xảy ra
 echo "Đang SSH vào EC2 instance..."
 ssh -i ~/.ssh/id_rsa $SSH_USER@$EC2_PUBLIC_IP "
     set -e &&
-    echo 'Đăng nhập Docker Registry...' &&
-    docker login -u $REGISTRY_USER -p $REGISTRY_PASS $REGISTRY_URL &&
     echo 'Kéo Docker image mới...' &&
     docker pull $IMAGE_NAME:$IMAGE_TAG &&
     echo 'Dừng và xóa container cũ nếu có...' &&
     docker stop react-app || true &&
     docker rm react-app || true &&
     echo 'Chạy container mới...' &&
-    docker run -d --name react-app -p 80:80 $IMAGE_NAME:$IMAGE_TAG
+    docker run -d --name react-app -p 80:80 $IMAGE_NAME:$IMAGE_TAG &&
+    echo 'Container đã được khởi động. Giữ container chạy liên tục...' &&
+    # Giữ container chạy vĩnh viễn
+    while true; do
+        sleep 60 # Duy trì hoạt động để container không bị dừng
+    done
 "
 
 echo "Triển khai thành công!"
-
