2ae8ce1e2dfde152f2e3595be99b2211b858528b
JonasNguyen <assasinhacker1@gmail.com>
2024-11-21 13:26:24 +0700

    Update file .gitlab-ci.yml

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,40 @@
-# Build stage
+# # Build stage
+# FROM node:16 AS build
+# WORKDIR /app
+# COPY package.json package-lock.json ./
+# RUN npm install
+# COPY . .
+# RUN npm run build
+# RUN ls -l /app/dist
+
+# # Serve stage
+# FROM nginx:alpine
+# COPY --from=build /app/dist /usr/share/nginx/html
+# EXPOSE 80
+# CMD ["nginx", "-g", "daemon off;"]
+
+# Sử dụng Node.js để build ứng dụng
 FROM node:16 AS build
+
+# Set thư mục làm việc trong container
 WORKDIR /app
+
+# Sao chép package.json và cài đặt dependencies
 COPY package.json package-lock.json ./
 RUN npm install
-COPY . .
-RUN npm run build
-RUN ls -l /app/dist
 
-# Serve stage
+# Sao chép toàn bộ mã nguồn vào container
+COPY . .
+
+# Build ứng dụng React
+RUN npm run build
+
+# Sử dụng nginx để phục vụ ứng dụng React đã build
 FROM nginx:alpine
-COPY --from=build /app/dist /usr/share/nginx/html
+
+# Sao chép build output từ image build vào nginx
+COPY --from=build /app/build /usr/share/nginx/html
+
+# Mở port 80 để phục vụ ứng dụng
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,22 @@ EC2_PUBLIC_IP=52.201.131.187
 IMAGE_NAME=nekrosj47/projectcicd
 IMAGE_TAG=react-app-1.0
 
-# Cấu hình SSH
-SSH_USER="ec2-user" # hoặc tên người dùng tương ứng với hệ điều hành EC2 của bạn
-PRIVATE_KEY_PATH="~/.ssh/id_rsa"
+# Cài đặt Docker và chạy ứng dụng
+ssh -i ~/.ssh/id_rsa ec2-user@$EC2_PUBLIC_IP <<SHEND
+    # Kiểm tra Docker có sẵn chưa, nếu chưa cài đặt Docker
+    if ! command -v docker &> /dev/null
+    then
+        echo "Docker not found. Installing Docker..."
+        sudo yum install -y docker
+        sudo service docker start
+        sudo usermod -aG docker ec2-user
+    else
+        echo "Docker is already installed."
+    fi
 
-# Kết nối SSH và triển khai Docker container
-ssh -i $PRIVATE_KEY_PATH $SSH_USER@$EC2_PUBLIC_IP << EOF
-    sudo docker pull $IMAGE_NAME:$IMAGE_TAG && \
-    sudo docker ps -q | xargs -r sudo docker stop && \
-    sudo docker ps -aq | xargs -r sudo docker rm && \
+    # Kéo Docker image và chạy container
+    sudo docker pull $IMAGE_NAME:$IMAGE_TAG
+    sudo docker ps -q | xargs -r sudo docker stop
+    sudo docker ps -aq | xargs -r sudo docker rm
     sudo docker run -d -p 80:80 $IMAGE_NAME:$IMAGE_TAG
-EOF
+SHEND
